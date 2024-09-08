5e36d7ac35a1aa9f84a7f18e80db898cd67f24f8
HunterrBoyy <shahiaaayus@gmail.com>
2024-09-08 12:45:45 +0545

    ch

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,27 +3,31 @@
 import React from "react";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
     description: "+977-9803342833",
+    link: "tel:+977 9803342833",
   },
   {
     icon: <FaEnvelope />,
     title: "Email",
     description: "shahiaaayus@gmail.com",
+    link: "mailto:shahiaaayus@gmail.com",
   },
   {
     icon: <FaMapMarkedAlt />,
     title: "Address",
     description: "Marudhoka, Kathmandu",
+    link: "https://maps.app.goo.gl/CEmhk1UzGNcFAYa49",
   },
 ];
 const page = () => {
   const inputStyle =
-    "flex h-[48px] rounded-md border-white/10 focus:border-[#00FF99] text-[#1c1c1c] font-light px-4 py-5 text-base placeholder:text-white/60 outline-none";
+    "flex h-[48px] rounded-md border-white/10 focus:border-[#00FF99] font-light bg-[#1c1c1c] px-4 py-5 text-base placeholder:text-white/60 outline-none";
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -35,7 +39,7 @@ const page = () => {
     >
       <div className="conatainer mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:h-[54%] order-2 xl:order-none ">
+          <div className="xl:w-[54%] order-2 xl:order-none ">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-[#00FF99]">Let's work together</h3>
               <p className="text-white/60">
@@ -68,7 +72,23 @@ const page = () => {
             </form>
           </div>
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-[#00FF99] rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <Link href={item.link} target="_blank">
+                        <h3 className="text-xl">{item.description}</h3>
+                      </Link>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
