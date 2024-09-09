9f02e221e05677f1aa7edc08f12a00953c4c6a1a
HunterrBoyy <shahiaaayus@gmail.com>
2024-09-09 12:52:43 +0545

    ch

--- src/app/page.js
+++ src/app/page.js
@@ -12,14 +12,17 @@ const Home = () => {
           <div className='text-center order-2 xl:order-none'>
             <span className='text-xl'>Frontend Developer</span>
             <h1 className='h1'>Hello I'm <br /> <span className='text-[#00FF99]'>Ayush Shahi</span></h1>
-            <p className='max-w-[500px] mb-9 text-white/80'>I am eager to create, learn and grow in th eworld of frontend development.</p>
+            <p className='max-w-[500px] mb-9 text-white/80'>I am eager to create, learn and grow in the world of frontend development.</p>
             <div className='flex flex-col items-center gap-8'>
+              <a href="/assets/ayush-cv.pdf" download="Resume">
+
               <button
               size="lg"
               className='uppercase flex items-center gap-2 px-3 py-2 rounded-[50px] text-[#00FF99] border border-[#00FF99]'>
                 <span>Download cv</span>
                 <FiDownload className='text-xl' />
               </button>
+                </a>
               <div className='mb-8'>
                 <Socials />
               </div>
