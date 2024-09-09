a695c649bb9373ef2235ea52d96f8d880d3f6713
HunterrBoyy <shahiaaayus@gmail.com>
2024-09-09 14:21:47 +0545

    ch

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
       center: true,
       padding: "15px",
       },
+      screens:{
+        sm:'640px',
+        md:'768px',
+        lg:'960px',
+        xl:'1200px',
+      },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
